5b91ea4542a68a87ce9a7261fc4e69207e1333fc
marialuciacastillog <148821609+marialuciacastillog@users.noreply.github.com>
2023-10-23 17:38:46 -0500

    Update operations.py

--- operations.py
+++ operations.py
@@ -23,3 +23,8 @@ def potencia(num_1, num_2):
     print(f'{num_1} ^ {num_2} is equal to {result}')
     return result
 
+def modulo(num_1, num_2):
+    result = int(num_1) % int(num_2)
+    print(f'{num_1} % {num_2} (residuo) is equal to {result}')
+    return result
+
