aa628fce8b29c60e79643ea8c0a2d1d38b26c0a9
marialuciacastillog <148821609+marialuciacastillog@users.noreply.github.com>
2023-10-23 17:23:53 -0500

    Update operations.py

--- operations.py
+++ operations.py
@@ -7,3 +7,9 @@ def rest(num_1, num_2):
     result = int(num_1) - int(num_2)
     print(f'{num_1} - {num_2} is equal to {result}')
     return result
+
+def multiply(num_1, num_2):
+    result = int(num_1) * int(num_2)
+    print(f'{num_1} * {num_2} is equal to {result}')
+    return result
+
