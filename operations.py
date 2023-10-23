7a4c3258dafa3bbbc0eb4411df423f7949f3bec0
marialuciacastillog <148821609+marialuciacastillog@users.noreply.github.com>
2023-10-23 17:17:15 -0500

    Update operations.py

--- operations.py
+++ operations.py
@@ -3,3 +3,7 @@ def add(num_1, num_2):
     print(f'{num_1} + {num_2} is equal to {result}')
     return result
 
+def rest(num_1, num_2):
+    result = int(num_1) - int(num_2)
+    print(f'{num_1} - {num_2} is equal to {result}')
+    return result
